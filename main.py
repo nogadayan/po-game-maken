e8b7edd514f0a03940b1bdcea4d6932d41f95ef8
Noga Dayan <110382@st-maartenscollege.nl>
2020-07-02 23:49:47 +0000

    while-loop van mogelijke antwoorden

--- main.py
+++ main.py
@@ -13,6 +13,28 @@ def play(word):
   tries = 5
   print("Let's play hangman!")
   print(display_hangman(tries))
+  print(word_completion)
+  print ("\n")
+  while not guessed and tries > 0:
+    guess = input("Please guess a letter or a word: ")
+    if len(guess) == 1 and guess.isalpha():
+      if guess in guessed_letters:
+        print("You already guessed this letter.", guess)
+      elif guess not in word:
+        print(guess, "is not in the word." )
+        tries -= 1
+        guessed_letters.append(guess)
+      else:
+        print("Good job!", guess, "is in the word!")
+        guessed_letters.append(guess)
+
+    elif len(guess) == len(word) and guess.isalpha():
+
+    else: 
+      print("Not a valid guess.")
+    print(display_hangman(tries))
+    print(word_completion)
+    print("\n")
 
 
 
