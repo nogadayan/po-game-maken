157df0f5792f929f7ffddbd8723cad34366bebe5
Noga Dayan <110382@st-maartenscollege.nl>
2020-07-02 22:33:36 +0000

    nieuwe folder: "words.py" gemaakt en daar mijn lijst van 10 woorden ingezet

---
+++ words.py
@@ -0,0 +1,13 @@
+word_list = [
+  'informatica',
+  'informatiekunde',
+  'spelletje',
+  'aardigheidje',
+  'scholier',
+  'fotografie',
+  'waardebepaling',
+  'specialiteit',
+  'verzekering',
+  'universiteit',
+  'heesterperk'
+]
