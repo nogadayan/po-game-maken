5e19f4309e309e36332a7e09c94820f5b7349423
Noga Dayan <110382@st-maartenscollege.nl>
2020-07-03 20:13:32 +0000

    kleine veranderingen in opmaak van de code en errors verbeteren pt.1veranderingen

--- main.py
+++ main.py
@@ -3,21 +3,21 @@ from words import word_list
 
 def get_word():
     word = random.choice(word_list)
-   return word.upper
+   return word.upper()
 
 
 def play(word):
-  word_completion = "_" * len(word)
-  guessed = False
-  guessed_letters = []
-  guessed_words = []
-  tries = 6
-  print("Let's play hangman!")
-  print(display_hangman(tries))
-  print(word_completion)
-  print ("\n")
-  while not guessed and tries > 0:
-    guess = input("Please guess a letter or a word: ")
+   word_completion = "_" * len(word)
+   guessed = False
+   guessed_letters = []
+   guessed_words = []
+   tries = 6
+   print("Let's play hangman!")
+   print(display_hangman(tries))
+   print(word_completion)
+   print ("\n")
+   while not guessed and tries > 0:
+        guess = input("Please guess a letter or a word: ").upper
     if len(guess) == 1 and guess.isalpha():
       if guess in guessed_letters:
         print("You already guessed this letter.", guess)
