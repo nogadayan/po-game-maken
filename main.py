3e0237f1ffe7637c73d564ee11863d98f2729e4c
Noga Dayan <110382@st-maartenscollege.nl>
2020-07-03 20:32:47 +0000

    aanpassingen opmaak code pt.2

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import random
 from words import word_list
 
+
 def get_word():
     word = random.choice(word_list)
    return word.upper()
@@ -18,42 +19,43 @@ def play(word):
    print ("\n")
    while not guessed and tries > 0:
         guess = input("Please guess a letter or a word: ").upper
-    if len(guess) == 1 and guess.isalpha():
-      if guess in guessed_letters:
-        print("You already guessed this letter.", guess)
-      elif guess not in word:
-        print(guess, "is not in the word." )
-        tries -= 1
-        guessed_letters.append(guess)
-      else:
-        print("Good job!", guess, "is in the word!")
-        guessed_letters.append(guess)
-        word_as_list = list(word_completion)
-        indices = [i for i, letter in enumerate(word) if letter == guess] 
-        for index in indices:
-          word_as_list[index] = guess
-        word_completion = "".join(word_as_list)
-        if "_" not in word_completion:
-          guessed = True
-    elif len(guess) == len(word) and guess.isalpha():
-      if guess in guessed_words:
-        print("You already guessed the word", guess)
-      elif guess != word:
-        print(guess, "is not in the word")
-        tries -= 1
-        guessed_words.append(guess)
-      else:
-        guessed = True
-        word_completion = word
+        if len(guess) == 1 and guess.isalpha():
+            if guess in guessed_letters:
+                print("You already guessed this letter.", guess)
+            elif guess not in word:
+                print(guess, "is not in the word." )
+                tries -= 1
+                guessed_letters.append(guess)
+            else:
+                print("Good job!", guess, "is in the word!")
+                guessed_letters.append(guess)
+                word_as_list = list(word_completion)
+                indices = [i for i, letter in enumerate(word) if letter == guess] 
+                for index in indices:
+                    word_as_list[index] = guess
+                word_completion = "".join(word_as_list)
+                if "_" not in word_completion:
+                    guessed = True
+        elif len(guess) == len(word) and guess.isalpha():
+            if guess in guessed_words:
+                print("You already guessed the word", guess)
+            elif guess != word:
+                print(guess, "is not in the word")
+                tries -= 1
+                guessed_words.append(guess)
+            else:
+                guessed = True
+                word_completion = word
+        else: 
+            print("Not a valid guess.")
+        print(display_hangman(tries))
+        print(word_completion)
+        print("\n")
+    if guessed:
+        print("Congratulations! You've guessed the word! You've won the game!")
     else: 
-      print("Not a valid guess.")
-    print(display_hangman(tries))
-    print(word_completion)
-    print("\n")
-  if guessed:
-    print("Congratulations! You've guessed the word! You've won the game!")
-  else: 
-    print("Sorry, you ran out of tries! The correct word was " + word + ". Maybe next time...")
+        print("Sorry, you ran out of tries! The correct word was " + word + ". Maybe next time...")
+
 
 def display_hangman(tries):
     stages = [
