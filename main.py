4881a61b824da40aed1b4af0fb91f90c8daf92b7
Noga Dayan <110382@st-maartenscollege.nl>
2020-07-03 00:02:43 +0000

    mogelijkheden "afgemaakt"

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ def get_word():
 
 
 def play(word):
-  word_completion = "-" * len(word)
+  word_completion = "_" * len(word)
   guessed = False
   guessed_letters = []
   guessed_words = []
@@ -27,7 +27,13 @@ def play(word):
       else:
         print("Good job!", guess, "is in the word!")
         guessed_letters.append(guess)
-
+        word_as_list = list(word_completion)
+        indices = [i for i, letter in enumerate(word) if letter == guess] 
+        for index in indices:
+          word_as_list[index] = guess
+        word_completion = "".join(word_as_list)
+        if "_" not in word_completion:
+          guessed = True
     elif len(guess) == len(word) and guess.isalpha():
 
     else: 
